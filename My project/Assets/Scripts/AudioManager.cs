037b5aef1ac962fb8f195f35d6aff380b64bdec8
TISamin <tahmidislamsamin@gmail.com>
2026-07-21 03:18:01 +0600

    Add skippable cutscene and looping music scripts

---
+++ My project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
---
+++ My project/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class CutsceneController : MonoBehaviour
+{
+    [SerializeField] private string videoFileName = "cutscene.mp4";
+    [SerializeField] private string nextSceneName = "MainMenu";
+
+    private VideoPlayer videoPlayer;
+    private bool isTransitioning = false;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    private void Start()
+    {
+        // Use streamingAssetsPath for WebGL and local compatibility
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+        
+        // Correct path format for WebGL vs standalone
+        #if UNITY_WEBGL && !UNITY_EDITOR
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = videoPath;
+        #else
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = "file://" + videoPath;
+        #endif
+
+        videoPlayer.loopPointReached += OnVideoFinished;
+        videoPlayer.Play();
+    }
+
+    private void Update()
+    {
+        // Listen for skip inputs
+        if (!isTransitioning && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape)))
+        {
+            TransitionToNextScene();
+        }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        TransitionToNextScene();
+    }
+
+    private void TransitionToNextScene()
+    {
+        if (isTransitioning) return;
+        isTransitioning = true;
+
+        videoPlayer.Stop();
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
